0bde7ad1de36bd8a5a984ab66fc39a547788b41e
shinta0806 <shinta.0806@gmail.com>
2021-08-26 23:39:31 +0900

    Ver 0.03 時点

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,32 +1,91 @@
-﻿using Livet;
+﻿// ============================================================================
+// 
+// アプリケーション
+// 
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+//
+// ----------------------------------------------------------------------------
+
+using Livet;
+
+using Shinta;
+
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+using System.Diagnostics;
+using System.Threading;
 using System.Windows;
+
+using Updater.Models.SharedMisc;
+using Updater.Models.UpdaterModels;
 
 namespace Updater
 {
 	public partial class App : Application
 	{
+		// ====================================================================
+		// private メンバー変数
+		// ====================================================================
+
+		// 多重起動防止用
+		// アプリケーション終了までガベージコレクションされないようにメンバー変数で持つ
+		private Mutex? _mutex;
+
+		// ====================================================================
+		// private メンバー関数
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// スタートアップ
+		// --------------------------------------------------------------------
 		private void Application_Startup(object sender, StartupEventArgs e)
 		{
+			// Livet コード
 			DispatcherHelper.UIDispatcher = Dispatcher;
-			//AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+			// 集約エラーハンドラー設定
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+			// 多重起動チェック
+			_mutex = CommonWindows.ActivateAnotherProcessWindowIfNeeded(Common.SHINTA + '_' + UpdConstants.APP_ID + '_' + UpdaterModel.Instance.EnvModel.ExeFullPath.Replace('\\', '/'));
+			if (_mutex == null)
+			{
+				throw new MultiInstanceException();
+			}
 		}
 
-		// Application level error handling
-		//private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-		//{
-		//    //TODO: Logging
-		//    MessageBox.Show(
-		//        "Something errors were occurred.",
-		//        "Error",
-		//        MessageBoxButton.OK,
-		//        MessageBoxImage.Error);
-		//
-		//    Environment.Exit(1);
-		//}
+		// --------------------------------------------------------------------
+		// 集約エラーハンドラー
+		// --------------------------------------------------------------------
+		private void CurrentDomain_UnhandledException(Object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+		{
+			if (unhandledExceptionEventArgs.ExceptionObject is MultiInstanceException)
+			{
+				// 多重起動の場合は何もしない
+			}
+			else
+			{
+				if (unhandledExceptionEventArgs.ExceptionObject is Exception excep)
+				{
+					// UpdaterModel 未生成の可能性があるためまずはメッセージ表示のみ
+					MessageBox.Show("不明なエラーが発生しました。アプリケーションを終了します。\n" + excep.Message + "\n" + excep.InnerException?.Message + "\n" + excep.StackTrace,
+							"エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+
+					try
+					{
+						// 可能であればログする。UpdaterModel 生成中に例外が発生する可能性がある
+						UpdaterModel.Instance.EnvModel.LogWriter.LogMessage(TraceEventType.Error, "集約エラーハンドラー：\n" + excep.Message + "\n" + excep.InnerException?.Message);
+						UpdaterModel.Instance.EnvModel.LogWriter.LogMessage(Common.TRACE_EVENT_TYPE_STATUS, "　スタックトレース：\n" + excep.StackTrace);
+					}
+					catch (Exception)
+					{
+						MessageBox.Show("エラーの記録ができませんでした。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+					}
+				}
+			}
+
+			Environment.Exit(1);
+		}
 	}
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -52,5 +52,55 @@ namespace Updater.ViewModels
 				UpdaterModel.Instance.EnvModel.LogWriter.ShowLogMessage(Common.TRACE_EVENT_TYPE_STATUS, "　スタックトレース：\n" + excep.StackTrace);
 			}
 		}
+
+		// ====================================================================
+		// protected メンバー関数
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// リソース解放
+		// --------------------------------------------------------------------
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+
+			if (_isDisposed)
+			{
+				return;
+			}
+
+			try
+			{
+				// アプリケーションの終了を通知
+				UpdaterModel.Instance.EnvModel.AppCancellationTokenSource.Cancel();
+
+				// 終了処理
+				//SaveExitStatus();
+				try
+				{
+					//Directory.Delete(YlCommon.TempFolderPath(), true);
+				}
+				catch
+				{
+				}
+
+				UpdaterModel.Instance.EnvModel.LogWriter.ShowLogMessage(Common.TRACE_EVENT_TYPE_STATUS, "終了しました：" + UpdConstants.APP_NAME_J + " "
+						+ UpdConstants.APP_VER + " --------------------");
+
+				_isDisposed = true;
+			}
+			catch (Exception excep)
+			{
+				UpdaterModel.Instance.EnvModel.LogWriter.ShowLogMessage(TraceEventType.Error, "メインウィンドウ破棄時エラー：\n" + excep.Message);
+				UpdaterModel.Instance.EnvModel.LogWriter.ShowLogMessage(Common.TRACE_EVENT_TYPE_STATUS, "　スタックトレース：\n" + excep.StackTrace);
+			}
+		}
+
+		// ====================================================================
+		// private メンバー変数
+		// ====================================================================
+
+		// Dispose フラグ
+		private Boolean _isDisposed;
 	}
 }
