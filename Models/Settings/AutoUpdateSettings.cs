c2985862128b08f146cb44ab26c109c82b87365d
shinta0806 <shinta.0806@gmail.com>
2021-11-27 13:25:47 +0900

    Ver 1.04 時点

--- Models/Settings/AutoUpdateSettings.cs
+++ Models/Settings/AutoUpdateSettings.cs
@@ -9,11 +9,8 @@
 // ----------------------------------------------------------------------------
 
 using Shinta;
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Updater.Models.Settings
 {
